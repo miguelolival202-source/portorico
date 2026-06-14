5c5d66a9da362b475bc56869b4158b925b70fbc0
miguelolival202-source <miguelolival202@gmail.com>
2026-06-14 20:40:58 -0300

    Fix formatting of set_page_config in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Quiz sobre Porto Rico", page_icon="🇵🇷")
+st.set_page_config(page_title="Quiz sobre Porto Rico")
 
 st.title("Quiz sobre Porto Rico")
 
